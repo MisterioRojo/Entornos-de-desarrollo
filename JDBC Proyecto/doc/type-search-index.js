f438f1914d69f4172a0094664bebc2cd3f8ce36d
Alejandro Rodriguez <104910719+MisterioRojo@users.noreply.github.com>
2022-05-28 18:43:28 +0200

    Add files via upload

---
+++ JDBC Proyecto/doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"DBManager","l":"DBManager"},{"p":"programa","l":"GestionClientes"},{"p":"programa","l":"GestionClientesIGU"}];updateSearchResults();
